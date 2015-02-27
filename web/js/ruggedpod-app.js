52dff6176aacc692ca8849d48659f296050769b5
Guillaume Giamarchi <guillaume.giamarchi@gmail.com>
2015-02-27 02:29:15 +0100

    Don't inline templates anymore

--- web/js/ruggedpod-app.js
+++ web/js/ruggedpod-app.js
@@ -1,48 +1,71 @@
-var ractive = new Ractive({
-    el: 'container',
-    template: '#template',
-    data: {
-        server_status: 'stopped',
-        power_state: 'OFF',
-        label_power_state: 'Power On'
-    }
-});
 
-listener = ractive.on({
-    'toggle-power-state': function () {
+var ractive;
+var templateIndex;
+var partials = {};
 
-        power_state = ractive.get('power_state');
-        if (power_state === 'ON') {
-            ruggedpod.get({
-                name: 'SetBladeAttentionLEDOff',
-                params: {
-                    bladeId: 1
-                },
-                error: function (error) {
-                    ractive.set('server_status', 'Error : ' + error);
-                },
-                success: function(data) {
-                    ractive.set('server_status', data.status);
-                    ractive.set('power_state', 'OFF');
-                    ractive.set('label_power_state', 'Power On');
-                }
-            });
+$.when(
+
+    $.get("/templates/index", function(index) {
+        templateIndex = index;
+    }),
+
+    $.get("/templates/main.html", function(html) {
+        partials.main = html;
+    })
+
+).then(function() {
+
+    var ractive = new Ractive({
+        el: 'container',
+        template: templateIndex,
+        partials: partials,
+        data: {
+            server_status: 'stopped',
+            power_state: 'OFF',
+            label_power_state: 'Power On'
         }
-        else if (power_state === 'OFF') {
-            ruggedpod.get({
-                name: 'SetBladeAttentionLEDOn',
-                params: {
-                    bladeId: 1
-                },
-                error: function (error) {
-                    ractive.set('server_status', 'Error : ' + error);
-                },
-                success: function(data) {
-                    ractive.set('server_status', data.status);
-                    ractive.set('power_state', 'ON');
-                    ractive.set('label_power_state', 'Power Off');
-                }
-            });
+    });
+
+    listener = ractive.on({
+        'toggle-power-state': function () {
+
+            power_state = ractive.get('power_state');
+            if (power_state === 'ON') {
+                ruggedpod.get({
+                    name: 'SetBladeAttentionLEDOff',
+                    params: {
+                        bladeId: 1
+                    },
+                    error: function (error) {
+                        ractive.set('server_status', 'Error : ' + error);
+                    },
+                    success: function(data) {
+                        ractive.set('server_status', data.status);
+                        ractive.set('power_state', 'OFF');
+                        ractive.set('label_power_state', 'Power On');
+                    }
+                });
+            }
+            else if (power_state === 'OFF') {
+                ruggedpod.get({
+                    name: 'SetBladeAttentionLEDOn',
+                    params: {
+                        bladeId: 1
+                    },
+                    error: function (error) {
+                        ractive.set('server_status', 'Error : ' + error);
+                    },
+                    success: function(data) {
+                        ractive.set('server_status', data.status);
+                        ractive.set('power_state', 'ON');
+                        ractive.set('label_power_state', 'Power Off');
+                    }
+                });
+            }
         }
-    }
+    });
+
+    // Display main page
+    ractive.set('page', 'main');
+
 });

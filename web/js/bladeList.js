ac776980556c4f0b9c985431cb357e5220100ffa
Guillaume Giamarchi <guillaume.giamarchi@gmail.com>
2015-03-02 23:55:24 +0100

    New page template
    * Replace On, Off and Force Off buttons by
      On/Off Short and On/Off Long to match the
      API
    * Replace individual pumps buttons by global
      ones
    * Add buttons to control all blades with a
      single call
    * Add cumulative power consomation

--- web/js/bladeList.js
+++ web/js/bladeList.js
@@ -1,47 +1,47 @@
 define(['ractive', 'client', 'notification'], function(ractive, client, notification){
 
     ractive.on({
-        'pump-on': function (event, id) {
+        'all-pumps-on': function (event) {
             client.get({
-                name: 'SetBladeAttentionLEDOn',
-                params: {
-                    bladeId: id
-                },
+                name: 'SetAllPumpsOn',
                 error: function (error) {
-                    notification.showError('Unable to start pump for blade ' + id);
+                    notification.showError('Unable to start pumps');
                 },
                 success: function(data) {
-                    notification.showSuccess('Pump of blade ' + id + ' has been successfully started');
+                    notification.showSuccess('Pumps has been successfully started');
                 }
             });
         },
-        'pump-off': function (event, id) {
+        'all-pumps-off': function (event) {
             client.get({
-                name: 'SetBladeAttentionLEDOff',
-                params: {
-                    bladeId: id
-                },
+                name: 'SetAllPumpsOff',
                 error: function (error) {
-                    notification.showError('Unable to start pump for blade ' + id);
+                    notification.showError('Unable to stop pumps');
                 },
                 success: function(data) {
-                    notification.showSuccess('Pump of blade ' + id + ' has been successfully stopped');
+                    notification.showSuccess('Pumps has been successfully stopped');
                 }
             });
         },
-        'blade-on': function (event, id) {
+        'all-blades-on-off-short': function (event) {
             notification.showInfo('Not yet implemented');
         },
-        'blade-off': function (event, id) {
+        'all-blades-on-off-long': function (event) {
+            notification.showInfo('Not yet implemented');
+        },
+        'all-blades-reset': function (event) {
+            notification.showInfo('Not yet implemented');
+        },
+        'blade-on-off-short': function (event, id) {
+            notification.showInfo('Not yet implemented');
+        },
+        'blade-on-off-long': function (event, id) {
             notification.showInfo('Not yet implemented');
         },
         'blade-reset': function (event, id) {
             notification.showInfo('Not yet implemented');
         },
-        'blade-force-off': function (event, id) {
-            notification.showInfo('Not yet implemented');
-        },
-        'serial-port-console': function (event, id) {
+        'serial-port-open-terminal': function (event, id) {
             notification.showInfo('Not yet implemented');
         }
     });

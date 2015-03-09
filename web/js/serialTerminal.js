f3c731de8b1110968d8c1632d8e5e1d428335e43
Guillaume Giamarchi <guillaume.giamarchi@gmail.com>
2015-03-09 02:00:04 +0100

    Handle power gauge on serial term page

--- web/js/serialTerminal.js
+++ web/js/serialTerminal.js
@@ -1,4 +1,7 @@
-define(['ractive', 'hasher', 'client', 'notification'], function(ractive, hasher, client, notification) {
+define(['ractive', 'hasher', 'gauge', 'client', 'notification'], function(ractive, hasher, gauge, client, notification) {
+
+    var powerGaugeRefresherId;
+    var powerGauge;
 
     ractive.on({
         'goto-blade-list': function (event) {
@@ -14,10 +17,30 @@ define(['ractive', 'hasher', 'client', 'notification'], function(ractive, hasher
     function initialize(params) {
         ractive.set('currentBladeId', parseInt(params['bladeId']))
         reloadSerialTerminal();
+        initializePowerGauge();
+    }
+
+    function initializePowerGauge() {
+        if (powerGauge === undefined) {
+            powerGauge = gauge.createPowerGauge('gauge-power-current-blade');
+        }
+
+        powerGaugeRefresherId = setInterval(function () {
+            // TODO Replace by a real call API
+            var delta = [-10, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 10];
+            var power = 125 + Math.floor(Math.random() * delta.length);
+
+            powerGauge.refresh(power);
+        }, 1000);
+    }
+
+    function finalize() {
+        clearInterval(powerGaugeRefresherId);
     }
 
     return {
-        initialize: initialize
+        initialize: initialize,
+        finalize: finalize
     }
 
 });

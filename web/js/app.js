RuggedPOD/ruggedpod-web

commit 02ecdd3a64fed2a4f5b10c05e432117ea9d5fd01

No longer hardcode blades information
Now blades information are coming from the
database thru the API.

web/js/app.js
@@ -46,36 +46,6 @@ define('ractive', ['Ractive',
             configuration: configurationTpl,
             documentation: documentationTpl,
             serialTerminal: serialTerminalTpl
-        },
-        data: {
-            notification: null,
-            currentBladeId: null,
-            blades: [
-                {
-                    id: 1,
-                    name: null,
-                    description: null,
-                    power: 0
-                },
-                {
-                    id: 2,
-                    name: null,
-                    description: null,
-                    power: 0
-                },
-                {
-                    id: 3,
-                    name: null,
-                    description: null,
-                    power: 0
-                },
-                {
-                    id: 4,
-                    name: null,
-                    description: null,
-                    power: 0
-                }
-            ]
         }
     });
 });

web/js/bladeList.js
@@ -134,43 +134,42 @@ define(['ractive', 'hasher', 'gauge', 'client', 'notification'], function(ractiv
     }
 
     function initialize(params) {
-        var blades = ractive.get('blades');
+
         var powerGauges = {};
 
-        for (var i = 0 ; i < blades.length ; i++) {
-            if (powerGauges[blades[i].id] === undefined) {
-                powerGauges[blades[i].id] = gauge.createPowerGauge('gauge-power-' + blades[i].id);
-            }
-        }
-
-        powerGaugeRefresherId = setInterval(function () {
-            updatePowerBladesData();
+        var refresh = function(blades) {
+            ractive.set('blades', blades);
             for (var i = 0 ; i < blades.length ; i++) {
-                powerGauges[blades[i].id].refresh(blades[i].power);
+                if (powerGauges[blades[i].id] === undefined) {
+                    powerGauges[blades[i].id] = gauge.createPowerGauge('gauge-power-' + blades[i].id);
+                }
             }
-        }, 1000);
+            for (i = 0 ; i < blades.length ; i++) {
+                powerGauges[blades[i].id].refresh(blades[i].consumption);
+            }
+        };
 
-    }
-
-    function updatePowerBladesData() {
-        var blades = ractive.get('blades');
         client.http({
             path: '/blades',
             method: 'GET',
             error: function (error) {
+                notification.showError('Unable to retrieve blades information');
             },
-            success: function(data) {
-                var blades = ractive.get('blades');
-                var bladeMap = {};
-                for (var i = 0 ; i < blades.length ; i++) {
-                    bladeMap[blades[i].id] = blades[i];
-                }
-                for (i = 0 ; i < data.length ; i++) {
-                    var bladeId = data[i].id;
-                    bladeMap[bladeId].power = data[i].consumption;
-                }
+            success: function(blades) {
+                refresh(blades);
             }
         });
+
+        powerGaugeRefresherId = setInterval(function () {
+            client.http({
+                path: '/blades',
+                method: 'GET',
+                success: function(blades) {
+                    refresh(blades);
+                }
+            });
+        }, 2000);
+
     }
 
     function finalize(params) {

commit 005caa7b6f6fb592bdd7f30681d6c4befb8aa67c

Fix bug top menubar cannot be click on first load
The bar looks like freezed. There's actually an
invisible notification on top.

This is a regression introduced in 02ecdd3.

web/js/app.js
@@ -46,6 +46,9 @@ define('ractive', ['Ractive',
             configuration: configurationTpl,
             documentation: documentationTpl,
             serialTerminal: serialTerminalTpl
+        },
+        data: {
+            notification: null
         }
     });
 });